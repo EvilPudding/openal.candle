335e30167a39fd5dba7a7d3b42020944b680287c
EvilPudding <jota.rod@hotmail.com>
2018-07-21 21:19:38 +0100

    Fixed 3D sound

--- openal.c
+++ openal.c
@@ -2,6 +2,8 @@
 #include "sound.h"
 
 #include <systems/sauces.h>
+#include <components/camera.h>
+#include <components/node.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +11,11 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 #include <AL/alut.h>
+#include <AL/alext.h>
+
+
+static LPALCGETSTRINGISOFT alcGetStringiSOFT;
+static LPALCRESETDEVICESOFT alcResetDeviceSOFT;
 
 void *sound_loader(const char *path, const char *name, uint ext)
 {
@@ -37,36 +44,108 @@ void c_openal_init(c_openal_t *self)
 	}
 	alutInit(0, NULL);
 
+	alDistanceModel(AL_INVERSE_DISTANCE_CLAMPED);
+
+	if(!alcIsExtensionPresent(self->device, "ALC_SOFT_HRTF"))
+	{
+		fprintf(stderr, "Error: ALC_SOFT_HRTF not supported\n");
+		exit(1);
+	}
+#define LOAD_PROC(d, x)  ((x) = alcGetProcAddress((d), #x))
+    LOAD_PROC(self->device, alcGetStringiSOFT);
+    LOAD_PROC(self->device, alcResetDeviceSOFT);
+#undef LOAD_PROC
+
+/* Enumerate available HRTFs, and reset the device using one. */
+	ALCint num_hrtf;
+	/* const char *hrtfname = "default-48000"; */
+	const char *hrtfname = NULL;
+	ALCint hrtf_state;
+
+    alcGetIntegerv(self->device, ALC_NUM_HRTF_SPECIFIERS_SOFT, 1, &num_hrtf);
+
+    if(!num_hrtf) printf("No HRTFs found\n");
+    else
+    {
+        ALCint attr[5];
+        ALCint index = -1;
+        ALCint i;
+
+        printf("Available HRTFs:\n");
+        for(i = 0; i < num_hrtf; i++)
+        {
+            const ALCchar *name = alcGetStringiSOFT(self->device, ALC_HRTF_SPECIFIER_SOFT, i);
+            printf("    %d: %s\n", i, name);
+
+            if(hrtfname && strcmp(name, hrtfname) == 0) index = i;
+        }
+
+        i = 0;
+        attr[i++] = ALC_HRTF_SOFT;
+        attr[i++] = ALC_TRUE;
+        if(index == -1)
+        {
+            if(hrtfname) printf("HRTF \"%s\" not found\n", hrtfname);
+            printf("Using default HRTF...\n");
+        }
+        else
+        {
+            printf("Selecting HRTF %d...\n", index);
+            attr[i++] = ALC_HRTF_ID_SOFT;
+            attr[i++] = index;
+        }
+        attr[i] = 0;
+
+        if(!alcResetDeviceSOFT(self->device, attr))
+            printf("Failed to reset device: %s\n", alcGetString(self->device, alcGetError(self->device)));
+	}
+	alcGetIntegerv(self->device, ALC_HRTF_SOFT, 1, &hrtf_state);
+	if(!hrtf_state) printf("HRTF not enabled!\n");
+	else
+	{
+		const ALchar *name = alcGetString(self->device, ALC_HRTF_SPECIFIER_SOFT);
+		printf("HRTF enabled, using %s\n", name);
+	}
 
 	sauces_loader(ref("wav"), sound_loader);
 }
 
+int c_openal_update(c_openal_t *self)
+{
+	ct_t *cameras = ecm_get(ref("camera"));
+	c_camera_t *camera = (c_camera_t*)ct_get_nth(cameras, 0);
+	if(!camera) return CONTINUE;
+	c_node_t *nc = c_node(camera);
+	c_node_update_model(nc);
+
+	vec3_t pos = mat4_mul_vec4(nc->model, vec4(0.0f, 0.0f, 0.0f, 1.0f)).xyz;
+	vec3_t at = vec3_norm(mat4_mul_vec4(nc->model,
+				vec4(0.0f, 0.0f, -1.0f, 0.0f)).xyz);
+	vec3_t up = vec3_norm(mat4_mul_vec4(nc->model,
+				vec4(0.0f, 1.0f, 0.0f, 0.0f)).xyz);
+
+	ALfloat listenerOri[] = { at.x, at.y, at.z, up.x, up.y, up.z };
+	alListener3f(AL_POSITION, pos.x, pos.y, pos.z);
+	alListenerfv(AL_ORIENTATION, listenerOri);
+	alListener3f(AL_VELOCITY, 0, 0, 0);
+
+	ALCenum error;
+	error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__);
+
+	return CONTINUE;
+}
+
 c_openal_t *c_openal_new()
 {
-	ALCenum error;
 
 	c_openal_t *self = component_new("openal");
 
-	ALfloat listenerOri[] = { 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f };
-	alListener3f(AL_POSITION, 0, 0, 1.0f);
-	error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__);
-	alListener3f(AL_VELOCITY, 0, 0, 0);
-	error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__);
-	alListenerfv(AL_ORIENTATION, listenerOri);
-	error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__);
-
-/* 	ALuint source_state; */
-/* 	do */
-/* 	{ */
-/* 		alGetSourcei(source, AL_SOURCE_STATE, &source_state); */
-/* 		error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__); */
-/* 	} */
-/* 	while (source_state == AL_PLAYING); */
+	c_openal_update(self);
 
 	return self;
 }
 
-c_openal_t *c_openal_destroy(c_openal_t *self)
+void c_openal_destroy(c_openal_t *self)
 {
 	alcMakeContextCurrent(NULL);
 	alcDestroyContext(self->context);
@@ -75,6 +154,9 @@ c_openal_t *c_openal_destroy(c_openal_t *self)
 
 REG()
 {
-	ct_new("openal", sizeof(c_openal_t), c_openal_init, c_openal_destroy, 0);
+	ct_t *ct = ct_new("openal", sizeof(c_openal_t), c_openal_init,
+			c_openal_destroy, 0);
+
+	ct_listener(ct, WORLD, sig("world_update"), c_openal_update);
 }
 
--- speaker.c
+++ speaker.c
@@ -1,4 +1,9 @@
 #include "speaker.h"
+#include "openal.h"
+#include <components/node.h>
+#include <components/model.h>
+#include <components/mesh_gl.h>
+#include <components/sprite.h>
 #include <components/node.h>
 
 #include <stdio.h>
@@ -10,14 +15,32 @@
 
 #define FILENAME "resauces/0.wav"
 
+entity_t g_speaker = entity_null;
+
 static int c_speaker_update_position(c_speaker_t *self);
 
 void c_speaker_init(c_speaker_t *self)
 {
-
 	alGenSources((ALuint)1, &self->source);
+
+	alSourcei(self->source, AL_SOURCE_RELATIVE, AL_FALSE);
+	/* alSourcef(self->source, AL_ROLLOFF_FACTOR, 1); */
+	/* alSourcef(self->source, AL_REFERENCE_DISTANCE, 6); */
+	/* alSourcef(self->source, AL_MAX_DISTANCE, 15); */
+
 	ALCenum error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__);
 	c_speaker_update_position(self);
+
+
+	if(!g_speaker)
+	{
+		mat_t *m = mat_new("speaker");
+		mesh_t *mesh = mesh_new();
+		mesh_quad(mesh);
+		g_speaker = entity_new(c_node_new(), c_model_new(mesh, mat_new("speaker"), 0, 0));
+		c_node(&g_speaker)->ghost = 1;
+		c_model(&g_speaker)->xray = 1;
+	}
 }
 
 void c_speaker_set_gain(c_speaker_t *self, float gain)
@@ -39,10 +62,21 @@ static int c_speaker_update_position(c_speaker_t *self)
 	vec3_t p = c_node_local_to_global(nc, vec3(0, 0, 0));
 
 	alSource3f(self->source, AL_POSITION, p.x, p.y, p.z);
+	alSource3f(self->source, AL_VELOCITY, 0, 0, 0);
 	ALCenum error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__);
 	/* alSource3f(source, AL_VELOCITY, 0, 0, 0); */
 	/* error = alGetError(); if (error != AL_NO_ERROR) printf("error at %d\n", __LINE__); */
 	return CONTINUE;
+}
+
+void c_speaker_pause(c_speaker_t *self)
+{
+
+}
+
+void c_speaker_stop(c_speaker_t *self)
+{
+
 }
 
 void c_speaker_play(c_speaker_t *self, sound_t *sound, int loop)
@@ -71,10 +105,31 @@ c_speaker_t *c_speaker_destroy(c_speaker_t *self)
 	alDeleteSources(1, &self->source);
 }
 
+static int c_speaker_render_visible(c_speaker_t *self)
+{
+	shader_t *shader = vs_bind(sprite_vs());
+	if(!shader) return STOP;
+	c_node_t *node = c_node(self);
+	if(node)
+	{
+		c_node_update_model(node);
+#ifdef MESH4
+		shader_update(shader, &node->model, node->angle4);
+#else
+		shader_update(shader, &node->model);
+#endif
+	}
+	glDepthRange(0, 0.01);
+	c_mesh_gl_draw(c_mesh_gl(&g_speaker), 0);
+	glDepthRange(0.0, 1.00);
+	return CONTINUE;
+}
+
 REG()
 {
-	ct_t *ct = ct_new("speaker", sizeof(c_speaker_t), c_speaker_init, c_speaker_destroy, 1,
-			ref("node"));
+	ct_t *ct = ct_new("speaker", sizeof(c_speaker_t), c_speaker_init,
+			c_speaker_destroy, 1, ref("node"));
 	ct_listener(ct, ENTITY, sig("spacial_changed"), c_speaker_update_position);
+	ct_listener(ct, WORLD, sig("render_visible"), c_speaker_render_visible);
 }
 
