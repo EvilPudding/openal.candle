ccecf4d9e4b78dd4d3d466b9742d32ede50dfeb2
EvilPudding <jota.rod@hotmail.com>
2020-08-17 11:33:20 +0100

    Fixed include

--- listener.h
+++ listener.h
@@ -1,14 +1,13 @@
 #ifndef LISTENER_H
 #define LISTENER_H
 
-#include <ecs/ecm.h>
+#include "../candle/ecs/ecm.h"
 
 typedef struct c_listener_t
 {
 	c_t super;
 } c_listener_t;
 
-void ct_listener(ct_t *self);
 DEF_CASTER(ct_listener, c_listener, c_listener_t)
 
 c_listener_t *c_listener_new(void);
